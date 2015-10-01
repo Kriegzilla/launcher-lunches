1bc6d299a2b877e3671276e07498a11a1f73fca9
Ian Campbell <ianwcampbell.contact@gmail.com>
2015-10-01 18:41:46 -0400

    it sorta works now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :groups
+  has_many :lunches,
+    through: :groups
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
